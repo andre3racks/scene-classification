1627fc85ad8b92da7d3644fddcf6e619554bcfd5
Andreas Anglin <andre@w134-87-141-112.wireless.uvic.ca>
2020-03-06 13:35:05 -0800

    sift now returns keypoints as well as descriptors

--- src/visualVocab/sift.py
+++ src/visualVocab/sift.py
@@ -1,26 +1,24 @@
 import cv2
 import numpy as np
 
-# debugging purposes
-# data = {}
-# data['X_train'] = [cv2.imread("output2.jpg", cv2.IMREAD_GRAYSCALE)]
-
 # creates a list of lists of sift keypoints for the given input images
 
 def build_sift_descriptors(data):
-    keypoint_list = []
+    keypoint_list, descriptor_list = [], []
 
     sift = cv2.xfeatures2d.SIFT_create()
     for ex in data['X_train']:
-        kp = sift.detect(ex,None)
+        kp, descriptors = sift.detectAndCompute(ex,None)
         keypoint_list.append(kp)
+        descriptor_list.append(descriptors)
         # print keypoints on image
         img=cv2.drawKeypoints(ex, kp, np.array([]), flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
         cv2.imwrite('sift_keypoints.jpg',img)
 
-    
-
-    return keypoint_list
+    return keypoint_list, descriptor_list
 
 # debugging purposes
-# build_sift_descriptors(data)
+if __name__ == "__main__":
+    data = {}
+    data['X_train'] = [cv2.imread("output2.jpg", cv2.IMREAD_GRAYSCALE)]
+    build_sift_descriptors(data)
